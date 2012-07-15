cbf95d98fa5e51607994c3751788abf1479060d6
Hiromitsu Murakami <murakami-hiromitsu@kayac.com>
2012-07-15 14:34:27 +0900

    Initial Commit

---
+++ FountainPen/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FountainPen
+//
+//  Created by  on 12/07/15.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FountainPen/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FountainPen
+//
+//  Created by  on 12/07/15.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
