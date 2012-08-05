4f4d96f3a1687de5a580a4f17e3fad534238ffea
Hiromitsu Murakami <murakami-hiromitsu@kayac.com>
2012-08-06 06:45:07 +0900

    縦の線を揃える

--- FountainPen/FountainPen/UI/UIView+Frame.h
+++ FountainPen/FountainPen/UI/UIView+Frame.h
@@ -11,8 +11,14 @@
 @property (nonatomic) CGFloat w;
 @property (nonatomic) CGFloat h;
 
-- (void)x:(CGFloat)x y:(CGFloat)y;
-- (void)w:(CGFloat)w h:(CGFloat)h;
-- (void)x:(CGFloat)x y:(CGFloat)y w:(CGFloat)w h:(CGFloat)h;
+- (void)x:(CGFloat)x
+        y:(CGFloat)y;
 
+- (void)w:(CGFloat)w
+        h:(CGFloat)h;
+
+- (void)x:(CGFloat)x
+        y:(CGFloat)y
+        w:(CGFloat)w
+        h:(CGFloat)h;
 @end
--- FountainPen/FountainPen/UI/UIView+Frame.m
+++ FountainPen/FountainPen/UI/UIView+Frame.m
@@ -13,17 +13,13 @@
 - (void)setW:(CGFloat)w { [self w:w      h:self.h]; }
 - (void)setH:(CGFloat)h { [self w:self.w h:h     ]; }
 
-- (void)x:(CGFloat)x y:(CGFloat)y
-{
-    [self x:x y:y w:self.w h:self.h];
-}
+- (void)x:(CGFloat)x y:(CGFloat)y { [self x:x      y:y      w:self.w h:self.h]; }
+- (void)w:(CGFloat)w h:(CGFloat)h { [self x:self.x y:self.y w:w      h:h     ]; }
 
-- (void)w:(CGFloat)w h:(CGFloat)h
-{
-    [self x:self.x y:self.y w:w h:h];
-}
-
-- (void)x:(CGFloat)x y:(CGFloat)y w:(CGFloat)w h:(CGFloat)h
+- (void)x:(CGFloat)x
+        y:(CGFloat)y
+        w:(CGFloat)w
+        h:(CGFloat)h
 {
     self.frame = CGRectMake(x, y, w, h);
 }
