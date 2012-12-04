c8674262b478751da6cfbf065b37576c37992961
Hiromitsu Murakami <murakami-hiromitsu@kayac.com>
2012-12-04 20:10:09 +0900

    テストを修正

---
+++ FountainPenLogicTests/FountainPenLogicTests.h
@@ -0,0 +1,13 @@
+//
+//  FountainPenLogicTests.h
+//  FountainPenLogicTests
+//
+//  Created by Q on 12/12/04.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface FountainPenLogicTests : SenTestCase
+
+@end
---
+++ FountainPenLogicTests/FountainPenLogicTests.m
@@ -0,0 +1,32 @@
+//
+//  FountainPenLogicTests.m
+//  FountainPenLogicTests
+//
+//  Created by Q on 12/12/04.
+//
+//
+
+#import "FountainPenLogicTests.h"
+
+@implementation FountainPenLogicTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FountainPenLogicTests");
+}
+
+@end
